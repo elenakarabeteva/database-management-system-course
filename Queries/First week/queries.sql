3b1c38b1ff46afac21897fee3a1007d0d22e214f
Elena Karabeteva <e.karabeteva6@gmail.com>
2024-10-09 19:31:08 +0300

    forth task completed

--- Queries/First week/queries.sql
+++ Queries/First week/queries.sql
@@ -7,14 +7,21 @@ FROM Employee
 WHERE SALARY > 30000
 ORDER BY SALARY DESC;
 
---Task 02
+-- Task 02
 SELECT LASTNAME AS 'Last name', 
 	FIRSTNME AS 'First name',
 	WORKDEPT AS 'Department number'
 FROM Employee
 ORDER BY WORKDEPT DESC, LASTNAME DESC;
 
---Task 03
+-- Task 03
 SELECT DISTINCT EDLEVEL AS 'Education levels'
 FROM Employee
 ORDER BY EDLEVEL DESC;
+
+-- Task 04
+SELECT DISTINCT EMPNO AS 'Employee number', 
+	PROJNO AS 'Project number'
+FROM Emp_Act
+WHERE EMPNO <= '000100'
+ORDER BY EMPNO;
